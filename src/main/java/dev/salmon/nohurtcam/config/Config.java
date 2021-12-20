3ca981c7996d35d20ec1ebd53b790021910ea831
Wyvest <45589059+Wyvest@users.noreply.github.com>
2021-12-20 13:32:02 +0700

    add limit for hypixel + add updater

--- src/main/java/dev/salmon/nohurtcam/config/Config.java
+++ src/main/java/dev/salmon/nohurtcam/config/Config.java
@@ -1,6 +1,9 @@
 package dev.salmon.nohurtcam.config;
 
 import dev.salmon.nohurtcam.NoHurtCam;
+import dev.salmon.nohurtcam.gui.DownloadGui;
+import dev.salmon.nohurtcam.utils.Updater;
+import gg.essential.api.EssentialAPI;
 import gg.essential.vigilance.Vigilant;
 import gg.essential.vigilance.data.Property;
 import gg.essential.vigilance.data.PropertyType;
@@ -10,26 +13,45 @@ import java.io.File;
 public class Config extends Vigilant {
 
     @Property(
-            type = PropertyType.SLIDER, name = "Adjust Hurt Camera Effect",
-            description = "Adjust Minecraft's hurt animation.",
+            type = PropertyType.NUMBER, name = "Adjust Hurt Camera Effect",
+            description = "Adjust Minecraft's hurt animation.\nThis will automatically be limited to 6 on non-Skyblock Hypixel gamemodes. The default is 14.",
             category = "General",
-            min = 0,
             max = 100
     )
     public static int adjustHurtCam = 14;
 
     @Property(
-            type = PropertyType.SLIDER, name = "Adjust Hurt Camera Effect in Lava",
-            description = "Adjust Minecraft's hurt animation while in lava.",
+            type = PropertyType.NUMBER, name = "Adjust Hurt Camera Effect in Lava",
+            description = "Adjust Minecraft's hurt animation while in lava.\nThis will automatically be limited to 6 on non-Skyblock Hypixel gamemodes. The default is 14.",
             category = "General",
             subcategory = "Other",
-            min = 0,
             max = 100
     )
     public static int adjustHurtCamInLava = 14;
 
+    @Property(
+            type = PropertyType.SWITCH,
+            name = "Show Update Notification",
+            description = "Show a notification when you start Minecraft informing you of new updates.",
+            category = "Updater"
+    )
+    public static boolean showUpdate = true;
+
+    @Property(
+            type = PropertyType.BUTTON,
+            name = "Update Now",
+            description = "Update by clicking the button.",
+            category = "Updater"
+    )
+    public void update() {
+        if (Updater.shouldUpdate) EssentialAPI.getGuiUtil()
+                .openScreen(new DownloadGui());
+        else EssentialAPI.getNotifications()
+                .push(NoHurtCam.NAME, "No update had been detected at startup, and thus the update GUI has not been shown.");
+    }
+
     public Config() {
-        super(new File(NoHurtCam.modDir, "nohurtcam.toml"), "NoHurtCam");
+        super(new File(NoHurtCam.modDir, "nohurtcam.toml"), NoHurtCam.NAME);
         initialize();
     }
 }
---
+++ src/main/java/dev/salmon/nohurtcam/utils/Updater.java
@@ -0,0 +1,99 @@
+package dev.salmon.nohurtcam.utils;
+
+import com.google.gson.JsonObject;
+import dev.salmon.nohurtcam.NoHurtCam;
+import dev.salmon.nohurtcam.config.Config;
+import dev.salmon.nohurtcam.gui.DownloadGui;
+import gg.essential.api.EssentialAPI;
+import gg.essential.api.utils.Multithreading;
+import gg.essential.api.utils.WebUtil;
+import kotlin.Unit;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.fml.common.versioning.DefaultArtifactVersion;
+import org.apache.commons.lang3.StringUtils;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+
+public class Updater {
+    public static String updateUrl = "";
+    public static String latestTag;
+    public static boolean shouldUpdate = false;
+
+    public static void update() {
+        Multithreading.runAsync(() -> {
+            try {
+                JsonObject latestRelease = WebUtil.fetchJSON("https://api.github.com/repos/W-OVERFLOW/" + NoHurtCam.ID + "/releases/latest").getObject();
+                latestTag = latestRelease.get("tag_name").getAsString();
+                DefaultArtifactVersion currentVersion = new DefaultArtifactVersion(StringUtils.substringBefore(NoHurtCam.VER, "-"));
+                DefaultArtifactVersion latestVersion = new DefaultArtifactVersion(StringUtils.substringBefore(StringUtils.substringAfter(latestTag, "v"), "-"));
+                if (currentVersion.compareTo(latestVersion) >= 0) {
+                    return;
+                }
+                updateUrl = latestRelease.get("assets").getAsJsonArray().get(0).getAsJsonObject().get("browser_download_url").getAsString();
+                if (!updateUrl.isEmpty()) {
+                    if (Config.showUpdate) {
+                        EssentialAPI.getNotifications().push(NoHurtCam.NAME, NoHurtCam.NAME + " has a new update (" + latestTag + ")! Click here to download it automatically!", Updater::openUpdateGui);
+                    }
+                    shouldUpdate = true;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+    public static Unit openUpdateGui() {
+        EssentialAPI.getGuiUtil().openScreen(new DownloadGui());
+        return Unit.INSTANCE;
+    }
+
+    public static boolean download(String url, File file) {
+        if (file.exists()) return true;
+        url = url.replace(" ", "%20");
+        try {
+            WebUtil.downloadToFile(url, file, "Hytilities/" + NoHurtCam.VER);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return file.exists();
+    }
+
+    /**
+     * Adapted from RequisiteLaunchwrapper under LGPLv3
+     * https://github.com/Qalcyo/RequisiteLaunchwrapper/blob/main/LICENSE
+     */
+    public static void addShutdownHook() {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Opening Deleter task...");
+            try {
+                String runtime = getJavaRuntime();
+                if (Minecraft.isRunningOnMac) {
+                    Desktop.getDesktop().open(NoHurtCam.jarFile.getParentFile());
+                }
+                File file = new File(NoHurtCam.modDir.getParentFile(), "Deleter-1.2.jar");
+                Runtime.getRuntime()
+                        .exec("\"" + runtime + "\" -jar \"" + file.getAbsolutePath() + "\" \"" + NoHurtCam.jarFile.getAbsolutePath() + "\"");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            Thread.currentThread().interrupt();
+        }));
+    }
+
+    /**
+     * Gets the current Java runtime being used.
+     * @link https://stackoverflow.com/a/47925649
+     */
+    public static String getJavaRuntime() throws IOException {
+        String os = System.getProperty("os.name");
+        String java = System.getProperty("java.home") + File.separator + "bin" + File.separator +
+                (os != null && os.toLowerCase(Locale.ENGLISH).startsWith("windows") ? "java.exe" : "java");
+        if (!new File(java).isFile()) {
+            throw new IOException("Unable to find suitable java runtime at "+java);
+        }
+        return java;
+    }
+}
