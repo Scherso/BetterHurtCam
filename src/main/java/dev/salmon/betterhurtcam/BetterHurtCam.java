c30b0ce7fab93c5a86018d475ed7a94087a1c9a1
Wyvest <45589059+Wyvest@users.noreply.github.com>
2022-02-10 11:14:50 +0700

    2.1.1 add w-core fix mixin not working

--- src/main/java/dev/salmon/betterhurtcam/BetterHurtCam.java
+++ src/main/java/dev/salmon/betterhurtcam/BetterHurtCam.java
@@ -1,10 +1,9 @@
 package dev.salmon.betterhurtcam;
 
+import cc.woverflow.wcore.utils.Updater;
 import dev.salmon.betterhurtcam.command.BetterHurtCamCommand;
 import dev.salmon.betterhurtcam.config.BetterHurtCamConfig;
 import dev.salmon.betterhurtcam.utils.SkyblockUtils;
-import dev.salmon.betterhurtcam.utils.Updater;
-import gg.essential.vigilance.Vigilance;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
@@ -16,33 +15,21 @@ import java.io.File;
 @Mod(name = BetterHurtCam.NAME, version = BetterHurtCam.VER, modid = BetterHurtCam.ID)
 public class BetterHurtCam {
     public static final String NAME = "@NAME@", VER = "@VER@", ID = "@ID@";
-    public static File jarFile;
     public static File modDir = new File(new File(Minecraft.getMinecraft().mcDataDir, "W-OVERFLOW"), NAME);
     public static BetterHurtCamConfig config;
-    @Mod.Instance private static BetterHurtCam instance;
 
     @Mod.EventHandler
     protected void onFMLPreInitialization(FMLPreInitializationEvent event) {
         if (!modDir.exists()) modDir.mkdirs();
-        jarFile = event.getSourceFile();
+        Updater.INSTANCE.addToUpdater(event.getSourceFile(), NAME, ID, VER, "W-OVERFLOW/" + ID);
     }
 
     @Mod.EventHandler
     protected void onInitialization(FMLInitializationEvent event) {
         new BetterHurtCamCommand().register();
-        Vigilance.initialize();
         config = new BetterHurtCamConfig();
         config.preload();
         MinecraftForge.EVENT_BUS.register(new SkyblockUtils());
-        Updater.update();
-    }
-
-    public BetterHurtCamConfig getConfig() {
-        return config;
-    }
-
-    public static BetterHurtCam getInstance() {
-        return instance;
     }
 
 }
--- src/main/java/dev/salmon/betterhurtcam/config/BetterHurtCamConfig.java
+++ src/main/java/dev/salmon/betterhurtcam/config/BetterHurtCamConfig.java
@@ -1,9 +1,6 @@
 package dev.salmon.betterhurtcam.config;
 
 import dev.salmon.betterhurtcam.BetterHurtCam;
-import dev.salmon.betterhurtcam.gui.DownloadGui;
-import dev.salmon.betterhurtcam.utils.Updater;
-import gg.essential.api.EssentialAPI;
 import gg.essential.vigilance.Vigilant;
 import gg.essential.vigilance.data.Property;
 import gg.essential.vigilance.data.PropertyType;
@@ -12,21 +9,15 @@ import java.io.File;
 
 public class BetterHurtCamConfig extends Vigilant {
 
-    public BetterHurtCamConfig() {
-        super(new File(BetterHurtCam.modDir, "betterhurtcam.toml"), BetterHurtCam.NAME);
-        initialize();
-    }
-
     // Main
 
     @Property(
             type = PropertyType.SLIDER, name = "Adjust Hurt Camera Effect",
             description = "Adjust Minecraft's hurt animation.\nThis will automatically be limited to 6 on non-Skyblock Hypixel gamemodes. The default is 14.",
             category = "General",
-            min = 0,
             max = 100
     )
-    private int adjustHurtCam = 14;
+    public static int adjustHurtCam = 14;
 
     @Property(
             type = PropertyType.SLIDER,
@@ -34,10 +25,9 @@ public class BetterHurtCamConfig extends Vigilant {
             description = "Adjust Minecraft's hurt animation while in lava.\nThis will automatically be limited to 6 on non-Skyblock Hypixel gamemodes. The default is 14.",
             category = "General",
             subcategory = "Damage Types",
-            min = 0,
             max = 100
     )
-    private int adjustHurtCamInLava = 14;
+    public static int adjustHurtCamInLava = 14;
 
     @Property(
             type = PropertyType.SLIDER,
@@ -45,45 +35,12 @@ public class BetterHurtCamConfig extends Vigilant {
             description = "Adjust Minecraft's hurt animation while on fire.\nThis will automatically be limited to 6 on non-Skyblock Hypixel gamemodes. The default is 14.",
             category = "General",
             subcategory = "Damage Types",
-            min = 0,
             max = 100
     )
-    private int adjustHurtCamIfBurning = 14;
+    public static int adjustHurtCamIfBurning = 14;
 
-    // Updater
-
-    @Property(
-            type = PropertyType.SWITCH,
-            name = "Show Update Notification",
-            description = "Show a notification when you start Minecraft informing you of new updates.",
-            category = "Updater"
-    )
-    public static boolean showUpdate = true;
-
-    @Property(
-            type = PropertyType.BUTTON,
-            name = "Update Now",
-            description = "Update by clicking the button.",
-            category = "Updater"
-    )
-    public void update() {
-        if (Updater.shouldUpdate) EssentialAPI.getGuiUtil()
-                .openScreen(new DownloadGui());
-        else EssentialAPI.getNotifications()
-                .push(BetterHurtCam.NAME, "No update had been detected at startup, and thus the update GUI has not been shown.");
-    }
-
-    // Getters
-
-    public int getAdjustHurtCam() {
-        return adjustHurtCam;
-    }
-
-    public int getAdjustHurtCamInLava() {
-        return adjustHurtCamInLava;
-    }
-
-    public int getAdjustHurtCamIfBurning() {
-        return adjustHurtCamIfBurning;
+    public BetterHurtCamConfig() {
+        super(new File(BetterHurtCam.modDir, "betterhurtcam.toml"), BetterHurtCam.NAME);
+        initialize();
     }
 }
