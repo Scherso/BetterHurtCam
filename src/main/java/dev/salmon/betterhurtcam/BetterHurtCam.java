96f272945ba1640a62e03ff5b1d6f4e5a77d2b08
Scherso <samsporidis@gmail.com>
2022-05-12 18:41:40 -0400

    Essential -> Vigilance

--- src/main/java/dev/salmon/betterhurtcam/BetterHurtCam.java
+++ src/main/java/dev/salmon/betterhurtcam/BetterHurtCam.java
@@ -1,9 +1,13 @@
 package dev.salmon.betterhurtcam;
 
-import dev.salmon.betterhurtcam.command.BetterHurtCamCommand;
+import dev.salmon.betterhurtcam.command.Command;
 import dev.salmon.betterhurtcam.config.BetterHurtCamConfig;
+import net.minecraft.command.ICommand;
+import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+import java.util.Arrays;
 
 @Mod(name = BetterHurtCam.NAME, version = BetterHurtCam.VER, modid = BetterHurtCam.ID)
 public class BetterHurtCam {
@@ -13,11 +17,18 @@ public class BetterHurtCam {
     public static BetterHurtCam Instance;
     private BetterHurtCamConfig config;
 
+    public static void registerCommands(ICommand... command) {
+        Arrays.stream(command).forEachOrdered(ClientCommandHandler.instance::registerCommand);
+    }
+
     @Mod.EventHandler
     protected void onInitialization(FMLInitializationEvent event) {
-        new BetterHurtCamCommand().register();
         config = new BetterHurtCamConfig();
         config.preload();
+
+        registerCommands(
+                new Command()
+        );
     }
 
     public BetterHurtCamConfig getConfig() {
