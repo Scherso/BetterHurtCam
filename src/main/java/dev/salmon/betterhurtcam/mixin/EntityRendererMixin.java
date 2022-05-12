8bb72c32dadce051d2c1d05fcf01fffdfc0d5cb6
Scherso <samsporidis@gmail.com>
2022-05-12 17:49:39 -0400

    Remove Hypixel Limit

--- src/main/java/dev/salmon/betterhurtcam/mixin/EntityRendererMixin.java
+++ src/main/java/dev/salmon/betterhurtcam/mixin/EntityRendererMixin.java
@@ -1,38 +1,23 @@
 package dev.salmon.betterhurtcam.mixin;
 
-import dev.salmon.betterhurtcam.config.BetterHurtCamConfig;
-import dev.salmon.betterhurtcam.utils.HypixelUtils;
+import dev.salmon.betterhurtcam.BetterHurtCam;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.EntityRenderer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(EntityRenderer.class)
 public class EntityRendererMixin {
 
-    @Shadow
-    private Minecraft mc = Minecraft.getMinecraft();
-
     @ModifyConstant(method = "hurtCameraEffect", constant = @Constant(floatValue = 14f))
     private float multiplyShake(float original) {
-        float shake = getShake();
-        if (HypixelUtils.inHypixel && !HypixelUtils.inSkyblock) {
-            if (shake < 6) {
-                shake = 6;
-            }
-        }
-        return shake;
-    }
-
-    private float getShake() {
-        if (mc.thePlayer.isInLava())
-            return (float) BetterHurtCamConfig.adjustHurtCamInLava;
-        if (mc.thePlayer.isBurning())
-            return (float)BetterHurtCamConfig.adjustHurtCamIfBurning;
+        if (Minecraft.getMinecraft().thePlayer.isInLava())
+            return (float) BetterHurtCam.Instance.getConfig().getAdjustHurtCamInLava();
+        if (Minecraft.getMinecraft().thePlayer.isBurning())
+            return (float) BetterHurtCam.Instance.getConfig().getAdjustHurtCamIfBurning();
         else
-            return (float)BetterHurtCamConfig.adjustHurtCam;
+            return (float) BetterHurtCam.Instance.getConfig().getAdjustHurtCam();
     }
 
 }
